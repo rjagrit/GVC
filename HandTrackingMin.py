acdc849b63a4e696d4ab0a651d6f218c21e4172e
Jagrit <rjagrit.2k@gmail.com>
2023-06-28 21:13:08 +0530

    fixed typo

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -23,8 +23,8 @@ while True:
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 # print(id, cx, cy)
-                if id == 4:
-                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+                # if id == 4:
+                cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
 
--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -2,23 +2,20 @@ import cv2
 import mediapipe as mp
 import time
 
+
 class handDetector():
-    def __init__(self,
-                 mode=False,
-                 maxHands=2,
-                 detectionCon=0.5,
-                 trackCon=0.5):
-        self.mode = mode
-        self.maxHands = maxHands
+    def __init__(self, mode=False, maxHands=2,complex=1, detectionCon=0.5, trackCon=0.5):
+        self.mode= mode
+        self.maxHands= maxHands
+        self.complex = complex
         self.detectionCon = detectionCon
         self.trackCon = trackCon
 
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands,
-                                        self.detectionCon, self.trackCon)
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,self.complex,self.detectionCon,self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
 
-    def findHands(self, img, draw=True):  # print(results.multi_hand_Landmarks)
+    def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
 
@@ -26,45 +23,54 @@ class handDetector():
             for handLms in self.results.multi_hand_landmarks:
                 if draw:
                     self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
-
         return img
 
-    def findPosition(self, img, handNo=0, draw=True):
-        lmList=[]
-        if self.results.multi_hand_Landmarks:
-            myHand = self.results.multi_hand_landmarks[handNo]
-            for id, lm in enumerate(myHand.landmark):
-                #print(id, lm)
+    def findPosition(self, img, handNo=0, draw= True):
+        lmlist = []
+        if self.results.multi_hand_landmarks:
+            myhand=self.results.multi_hand_landmarks[handNo]
+
+            for id, lm in enumerate(myhand.landmark):
+                print(id, lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 # print(id, cx, cy)
-                lmList.append([id, cx, cyl])
+                lmlist.append([id,cx, cy])
                 if draw:
-                    cv2.circle(img,(cx,cy),15,(255,0,255),cv2.FILLED)
-        return lmList
+                    cv2.circle(img, (cx, cy), 10, (255, 0, 110), cv2.FILLED)
+
+        return lmlist
+
+
+    # cTime = time.time()
+    # fps=1 / ( cTime - pTime)
+    # pTime = cTime
+    #
+    # cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+
+
 
 def main():
-    pTime=0
-    cTime=0
-    cap=cv2.VideoCapture(0)
-    detector= handDetector()
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(0)
+    detector = handDetector()
+
     while True:
-        success , img = cap.read()
-        img = detector.findHands(img)
+        success, img = cap.read()
+        img= detector.findHands(img)
         lmlist= detector.findPosition(img)
         if len(lmlist)!=0:
             print(lmlist[4])
+        cTime = time.time()
+        fps=1 / ( cTime - pTime)
+        pTime = cTime
 
-        cTime=time.time()
-        fps = 1/ (cTime-pTime)
-        pTime=cTime
+        cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN, 3, (255,0,0), 3)
 
-        cv2.putText(img, str(int(fps)), (10,70), cv2.FRONT_HERSHEY_PLAN, 3, (255,0,255),3)
-        cv2.imshow("Image",img)
+        cv2.imshow("Image", img)
         cv2.waitKey(1)
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     main()
-
-
-
